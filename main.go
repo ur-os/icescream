4669de86757be74c198c3f65dd8bfc5822375213
22165381 <22165381@sigma.sbrf.ru@CAB-WSD-0002831>
2024-06-11 15:05:34 +0300

    collisiton

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"vkdefinition/pkg/authHandlers"
+	"vkdefinition/pkg/server"
+)
+
+func main() {
+	var httpAddr = flag.String("http", ":8085", "http listen address")
+
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.NewSyncLogger(logger)
+		logger = log.With(logger,
+			"serve", "auth",
+			"time:", log.DefaultTimestampUTC,
+			"caller", log.DefaultCaller,
+		)
+	}
+
+	flag.Parse()
+	var srv authHandlers.Auth
+	{
+		srv = authHandlers.NewServiceAuth(logger)
+	}
+
+	errs := make(chan error)
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	endpoints := server.MakeEndpointsAuth(srv)
+
+	go func() {
+		fmt.Println("listening on port", *httpAddr)
+		handler := server.NewHTTPServer(context.Background(), endpoints)
+		errs <- http.ListenAndServe(*httpAddr, handler)
+	}()
+
+	level.Error(logger).Log("exit", <-errs)
+}
